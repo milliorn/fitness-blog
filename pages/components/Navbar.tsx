14af2da53239ce5137ad611cd746f07fa26ee7b5
milliorn <scottmilliorn@gmail.com>
2023-03-29 15:35:11 -0700

    import

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(): JSX.Element {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
-      
+      <div></div>
     </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function Home(): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Navbar/>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
