48413836a40a265dc095e81d9efd0cde79fc545b
milliorn <scottmilliorn@gmail.com>
2023-03-29 19:44:15 -0700

    ListItem

--- app/components/Listitem.tsx
+++ app/components/Listitem.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import getFormattedDate from "@/lib/getFormattedDate";
 
 type Props = {
   post: BlogPost;
@@ -6,5 +7,18 @@ type Props = {
 
 export default function ListItem({ post }: Props): JSX.Element {
   const { id, title, date } = post;
-  return <div>ListItem</div>;
+  const formattedDate = getFormattedDate(date);
+
+  return (
+    <li className="mt-4 text-2xl dark:text-white/90">
+      <Link
+        className="underline hover:text-black/70 dark:hover:text-white"
+        href={`/posts/${id}`}
+      >
+        {title}
+      </Link>
+      <br />
+      <p className="text-sm mt-1">{formattedDate}</p>
+    </li>
+  );
 }
